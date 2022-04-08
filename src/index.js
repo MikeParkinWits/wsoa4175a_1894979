ce0923707705078722f784631f19c109f2ba52db
Michael Parkin <1894979@students.wits.ac.za>
2022-04-08 23:16:18 +0200

    Deployment Commit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 //Adding React Routing
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
