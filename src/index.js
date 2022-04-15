71d8aa2e5570db8a9816ee76347a94cdb533ff98
Michael Parkin <1894979@students.wits.ac.za>
2022-04-15 22:05:32 +0200

    Added react-snap

--- src/index.js
+++ src/index.js
@@ -7,12 +7,26 @@ import reportWebVitals from "./reportWebVitals";
 //Adding React Routing
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+import { hydrate, render } from "react-dom";
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement
+  );
+} else {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement
+  );
+}
+
+ReactDOM.render(document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
